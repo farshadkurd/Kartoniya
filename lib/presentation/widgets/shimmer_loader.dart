485f22d8c67558a73f9cf7a812c1fc750994c47e
farshadkurd <52835407+farshadkurd@users.noreply.github.com>
2026-08-09 01:28:23 +0330

    Create shimmer_loader.dart

---
+++ lib/presentation/widgets/shimmer_loader.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+import '../../../core/theme/app_theme.dart';
+
+class ShimmerCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Shimmer.fromColors(
+      baseColor: Colors.grey.shade300,
+      highlightColor: Colors.grey.shade100,
+      child: Container(
+        decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(25)),
+      ),
+    );
+  }
+}
